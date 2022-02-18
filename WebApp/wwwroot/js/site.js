fadb3da4eff4411bb499c0451a3bcf4cdb854cd1
Jayesh <jayesh@shardeen.com>
2022-02-18 16:10:43 +0530

    post ajax call

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -4,21 +4,56 @@
 // Write your JavaScript code.
 const apiUrl = 'https://localhost:7261/api';
 
-function createState(form) {
+function postForm(form,appendUrl) {
+   
+    const formData = new FormData(form)
+
     $.ajax({
-        url: apiUrl + `/StatesApi?name=asdsf`,
+        url: apiUrl + appendUrl,
         method: 'POST',
-        //data: JSON.stringify({ name: 'jayesh' }),
-        data: "{ name='jayesh' }",
+        data: JSON.stringify(Object.fromEntries(formData)),
         contentType: 'application/json',
 
     }).done(function (data) {
-        if (callback)
-            alert("Success");
-        else {
-            return data;
-        }
+        alert("Success");
+        return data;
+
     }).fail(function (data) {
         alert('Error : (' + data.responseJSON.message + '). Please try later.');
     });
 }
+
+
+    //    fetch(url, {
+    //        method: "POST",
+    //        headers: { "Content-Type": "application/json" },
+    //        body: JSON.stringify({ name: 'shubham' })
+    //    }).then(() => {
+    //        alert("New Blog Added");
+    //    })
+    //}
+
+
+    //const form = document.querySelector('form')
+    //form.addEventListener('submit', (e) => {
+    //    e.preventDefault()
+    //    url = apiUrl + `/StatesApi`;
+    //    const formData = new FormData(e.target)
+    //    const data = JSON.stringify(Object.fromEntries(formData));
+    //    console.log(Object.fromEntries(formData));
+    //    console.log(data);
+    //    console.log(JSON.stringify({ "name": "pawan582" }))
+    //    fetch(url, {
+    //        method: "POST",
+    //        headers: { "Content-Type": "application/json" },
+
+    //        //body: JSON.stringify({ "name": "pawan582" })
+    //        body: data
+    //        //body: Object.fromEntries(formData)
+    //    }).then(() => {
+    //        alert("New Blog Added");
+    //    })
+
+    //})
+
+
